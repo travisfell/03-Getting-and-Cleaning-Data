1a49b137989380e82dc657db276490aefbfd2c3b
Travis Fell <tfell2001@gmail.com>
2015-02-16 23:12:13 -0600

    latest code updates
    added code to combine both data sets and pull only needed columns

--- courseproject/run_analysis.R
+++ courseproject/run_analysis.R
@@ -22,6 +22,7 @@ subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = F
 # name the columns in each data set
 names(testResults) <- features[,2]
 names(trainResults) <- features[,2]
+names(activities) <- c("activityID", "activity")
 
 # append test and train subject ID's to the results sets
 testResults[,"subjectID"] <- subjectTest
@@ -32,11 +33,29 @@ trainResults[,"subjectID"] <- subjectTrain
 # 010.B Join the tables on the appropriate keys to create the tidy data needed for the rest of the script
 
 
-# 020. Extract the man and stdev for each measurement
 
 # 030. Give descriptive activity names to the activities in the data set
+# append testLabels to testResults to enable activity name matching
+testResults[,"activityID"] <- testLabels
+trainResults[,"activityID"] <- trainLabels
 
-# 040. Label the data set with descriptive variable names
+# append activity name based on activity ID
+testResults2 <- merge(activities, testResults, by.x = "activityID", by.y = "activityID")
+trainResults2 <- merge(activities, trainResults, by.x = "activityID", by.y = "activityID")
+
+# combine the test and train data sets (like a SQL union query)
+results <- rbind(testResults2, trainResults2)
+
+# 020. Extracts only the measurements on the mean and standard deviation for each measurement. 
+resultscols <- colnames(results) 
+aveStdCols <- grep("mean|std|activity", resultscols, ignore.case = TRUE)
+aveStdDetails <- results[,aveStdCols]
+
 
 # 050. From data set in step 4, create second, indepedent tidy
 # data set with the averages of each variable for each activity
+
+#aveStdSummary <- summarize(group_by(aveStdDetails, activity), lapply(aveStdDetails[,3:88], mean))
+aveStdColNames <- matrix(names(aveStdDetails[,3:88]))
+#aveStdSummary <- xtabs(mean~activity + aveStdColNames, data=aveStdDetails)
+#aveStdSummary <-summarize(group_by(aveStdDetails, activity), mean(aveStdColNames))
